67d31721830fb5c28af4eb444f459157f14ef0df
OIMA <09280502@ittoluca.edu.mx>
2014-12-10 23:20:12 -0600

    Iframe loaded checker

--- webapp/BIG_DATA_DASHBOARD/src/main/webapp/js/dashboard.js
+++ webapp/BIG_DATA_DASHBOARD/src/main/webapp/js/dashboard.js
@@ -30,3 +30,31 @@ function reloadFrame(address) {
     $("#iframe").attr("src", address);
 //    $('#iframe').contentWindow.location.reload(true);
 }
+
+function checkIframeLoaded() {
+    // Get a handle to the iframe element
+//     iframe = document.getElementById('i_frame');
+    var iframe = $('iframe');
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+    // Check if loading is complete
+    if (  iframeDoc.readyState  == 'complete' ) {
+
+        //iframe.contentWindow.alert("Hello");
+
+        iframe.contentWindow.onload = function(){
+            alert("I am loaded");
+        };
+
+        // The loading is complete, call the function we want executed once the iframe is loaded
+        afterLoading();
+        return;
+    } 
+
+    // If we are here, it is not loaded. Set things up so we check   the status again in 100 milliseconds
+    window.setTimeout('checkIframeLoaded();', 100);
+}
+
+function afterLoading(){
+    alert("I am here");
+}
