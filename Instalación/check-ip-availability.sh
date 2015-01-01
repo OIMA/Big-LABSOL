ab53ea66e36ad34c7c5e0ac7e14e7e35eb251756
OIMA <09280502@ittoluca.edu.mx>
2015-01-01 12:39:34 -0600

    HDP 2.2 updated

--- Instalación/check-ip-availability.sh
+++ Instalación/check-ip-availability.sh
@@ -1,13 +1,6 @@
-#Este script nos sirve para visualizar un rango de ips disponibles para nuestro cluster
-
-for IP in 10.39.224.{1..254}; 
+for IP in 10.39.224.{200..254}; 
     do 
         if ping $IP -c 1 > /dev/null; 
             then echo $IP alive; 
         else echo $IP dead; fi; 
     done
-
-
-#scrolling="no"
-#frame-border="0"
-#border="0"
--- Instalación/install-hortonworks.sh
+++ Instalación/install-hortonworks.sh
@@ -9,10 +9,45 @@ make cmake automake
 fuse
 wget scp
 openssh openssh-server openssh-clients
-mysql mysql-server
 yum-utils createrepo
 yum-priorities yum-plugin-priorities
 ;
 
 yum search $REQUIRED_PACKAGES
 rpm -qa | grep $REQUIRED_PACKAGES 
+
+#Instalación R
+#
+# wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+# yum localinstall epel-release-6-8.noarch.rpm
+# yum install R
+
+echo "********************************************************************";
+echo "* Para la instalación de los servicios de Big Data en el clúster,  *";
+echo "* es necesario llevar a cabo los siguientes pasos, seleccione      *";
+echo "* una de las siguientes opciones.                                  *";
+echo "*                                                                  *";
+echo "* 0.- Configurar ip estática en cada nodo del clúster              *";
+echo "* 1.- Configurar archivos de hosts en cada nodo del clúster        *";
+echo "* 2.- Instalación automática de java                               *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*                                                                  *";
+echo "*********************************************************************";
+
+#Install and configure mysql database
+
+#Configure NTP
+
+#Disable 
+
