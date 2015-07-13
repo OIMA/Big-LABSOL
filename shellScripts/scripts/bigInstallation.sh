5ed1cab8a82a10af7f7e24ea64a2cf7846539281
Javier A. Gonzalez <helojagt@gmail.com>
2015-07-12 20:23:30 -0500

    Users and Grups creations

--- shellScripts/scripts/bigInstallation.sh
+++ shellScripts/scripts/bigInstallation.sh
@@ -355,6 +355,25 @@ setDirectoriesVariables(){
     cat directories.sh >> ~/.bash_profile
 }
 
+setUsersVariables(){
+    . usersAndGroups.sh
+    echo '' >> ~/.bash_profile
+    cat usersAndGroups.sh >> ~/.bash_profile
+}
+
+createUsersAndGroups(){
+    #Status: Testing
+    
+    for var in $HDFS_USER $YARN_USER $MAPRED_USER $PIG_USER $HIVE_USER $WEBHCAT_USER $HBASE_USER $ZOOKEEPER_USER $OOZIE_USER $BIGDATA_USER
+    do
+	#Add user to the group
+	sudo useradd -G $HADOOP_GROUP $var
+	
+	#Set a password for each user
+	passwd $var
+    done
+}
+
 # =============================================================================
 # Script Main
 # =============================================================================
